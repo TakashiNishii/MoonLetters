1972291b5c34413fac160253ba0a2e38bb22f972
TakashiNishii <takashi@geeklys.com>
2024-06-07 16:34:50 -0400

    :art: Go back button

--- app/[sign]/page.tsx
+++ app/[sign]/page.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "../components/Header";
 import Horoscope from "../components/Horoscope";
 import { getDataHoroscope } from "../components/functions/getDataHoroscope";
+import Link from "next/link";
+import { ArrowLeftCircleIcon } from "@heroicons/react/20/solid";
 
 interface HoroscopePageProps {
   params: {
@@ -15,6 +17,13 @@ const HoroscopePage = async ({ params: { sign } }: HoroscopePageProps) => {
     <div className="flex min-h-screen flex-col items-center p-6 md:p-24">
       <Header />
       <Horoscope signSelected={sign} horoscopeData={horoscopeData} />
+      <Link
+        href="/"
+        className="font-extrabold text-3xl text-neutral flex gap-2"
+      >
+        <ArrowLeftCircleIcon className="w-10 h-10 cursor-pointer" />
+        Go back
+      </Link>
     </div>
   );
 };
